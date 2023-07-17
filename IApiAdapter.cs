5b0419741e6d3fd30fd2c25a01a6c88ed50d727c
Иван А. Ефимов <60976@UMP.local>
2023-07-17 13:55:39 +0400

    update assembly info for packaging

--- IApiAdapter.cs
+++ IApiAdapter.cs
@@ -7,18 +7,14 @@ namespace UMP.Loodsman.Adapters
     public interface IApiAdapter
     {
         ISimpleAPI2 Api { get; }
-
         void RunMethod(string methodName, params object[] arguments);
         T RunMethod<T>(string methodName, params object[] arguments);
         IDataSet GetDataSet(string methodName, params object[] arguments);
         Task RunMethodAsync(string methodName, params object[] arguments);
         Task RunMethodAsync(CancellationToken token, string methodName, params object[] arguments);
         Task<T> RunMethodAsync<T>(string methodName, params object[] arguments);
-
         Task<T> RunMethodAsync<T>(CancellationToken token, string methodName, params object[] arguments);
-
         Task<IDataSet> GetDataSetAsync(string methodName, params object[] arguments);
-
         Task<IDataSet> GetDataSetAsync(CancellationToken token, string methodName, params object[] arguments);
     }
 }
